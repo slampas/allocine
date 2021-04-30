e10d1edc264d52a2e764bf62a1ef7c0587e96d49
Fabien Bidault <fabien.bidault.pro@gmail.com>
2021-04-30 12:52:34 +0200

    docs: test de la fonction anonyme

---
+++ scripts/identite.js
@@ -0,0 +1,4 @@
+// fonction: qui permet au chargement de la page d'effectuer l'ajout d'information sur l'identite de l'auteur.
+window.addEventListener("load", function(event) {
+    console.log("Toutes les ressources sont chargées !");
+});
