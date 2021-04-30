b3d9e633fc10e1a37e82f4320f7169c363430823
Amir <amir@baziz.fr>
2021-04-30 14:33:31 +0200

    slides with new movies

--- scripts/cinema.js
+++ scripts/cinema.js
@@ -1,6 +1,7 @@
-var slide = new Array("telechargement.jfif","adaptation-du-jeu.jpg", "BLACK-MESSIAH.jpg");
-var titre = new Array ("Titre 1","Titre 2","Titre 3");
-var description = new Array ("  Pierre Lapin est de retour pour de nouvelles aventures à découvrir le 30 juin prochain "," lorem lorem lorem lorem prochain ");
+var slide = new Array("batman.JPG","lucy.jpg", "Avengers.jpg");
+var titre = new Array ("BATMAN","Lucy","Avengers");
+var description = new Array (" Alors que l'on attend The Batman de Matt Reeves et que Joker cartonne toujours dans les salles de cinéma, on ne peut oublier la trilogie The Dark Knight de Christopher Nolan. ",
+"C'est mieux qu'un cours de biologie,  affirme un autre spectateur à l'AFP à la sortie  de la première projection française de Lucy.","  Nick Fury, désormais directeur de l'organisation de défense terrestre SHIELD, veut monter une équipe de super-héros ");
 var numerotation = new Array ("1/2","2/3","3/3");
 var numero = 0;
 
@@ -12,5 +13,7 @@ function ChangeSlide(sens) {
         numero = 0;
     document.getElementById("slide").src ="../assets/"+slide[numero];
     document.getElementById("titre").innerHTML = titre[numero];
-    document.getElementById("titre").innerHTML = description[numero];
+    document.getElementById("description").innerHTML = description[numero];
+    
+    document.getElementById("numero").innerHTML = numerotation[numero];
 }
