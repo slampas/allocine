1fd89812692d955b5a3b520c5bfdf6f800a5473a
Fabien Bidault <fabien.bidault.pro@gmail.com>
2021-04-30 13:18:35 +0200

    docs: ajout de la fonction anonyme pour intégrer l'identite d'une personne

--- scripts/identite.js
+++ scripts/identite.js
@@ -1,4 +1,11 @@
-// fonction: qui permet au chargement de la page d'effectuer l'ajout d'information sur l'identite de l'auteur.
-window.addEventListener("load", function(event) {
-    console.log("Toutes les ressources sont chargées !");
+var identite = { metier: "R", nomdenaissance: "O", nationalite: "A", naissance: "I", age: "1", biographie: "Lorem Ipsum" };
+
+// fonction: qui permet au chargement de la page d'intégrer les informations sur l'identite d'une personne.
+window.addEventListener("load", function (event) {
+    document.getElementById("metier").innerHTML = "Métier " + identite["metier"];
+    document.getElementById("nomdenaissance").innerHTML = "Nom de naissance  " + identite["nomdenaissance"];
+    document.getElementById("nationalite").innerHTML = "Nationalité  " + identite["nationalite"];
+    document.getElementById("naissance").innerHTML = "Naissance " + identite["naissance"];
+    document.getElementById("age").innerHTML = "Age " + identite["age"];
+    document.getElementById("biographie").innerHTML = identite["biographie"];
 });
